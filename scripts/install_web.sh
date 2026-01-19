cb25db47e2b534d5ed3f72b4d86c7597e39358ac
虜漣踪 <lorenzo.bellier@gmail.com>
2026-01-19 16:22:39 +0100

    Add script to install Apache server and configure firewall

--- scripts/install_web.sh
+++ scripts/install_web.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+#Install Apache Server and enable HTTPS
 
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+apt update
+apt install -y apache2
+systemctl enable apache2
+systemctl start apache2
+
+#enable UFW and allow HTTP/HTTPS
+ufw allow 'Apache Full'
+ufw enable
+
+echo "Web server installed and basic firewall configured"
